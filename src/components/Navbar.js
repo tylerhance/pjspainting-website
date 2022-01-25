0490e1afa74ba761cefdce40b2ee8f0c00d40670
Tyler Hance <tyler.hance@gmail.com>
2022-01-24 17:20:29 -0800

    Adding features and tiles to navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+
+
+function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav-container">
+      <li>
+        <a
+          href="#about"
+          onClick={() => handlePageChange("About")}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          About 
+        </a>
+      </li>
+      <li>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange("Portfolio")}
+          className={
+            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li>
+        <a
+          href="#resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Services" ? "nav-link active" : "nav-link"}
+        >
+          Services
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+
+  
+}
+
+export default Navbar;
+
+
+
+
+
