58390183ce9d6befa9b89615c5699201388f0510
Tyler Hance <tyler.hance@gmail.com>
2022-01-27 17:36:09 -0800

    Styling and writing the contact form/contact us section

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "../ContactForm";
+import "../../styles/About.css";
+
+export default function Contact() {
+    return(
+        <div>
+            <h1>Contact Us</h1>
+            <ContactForm />
+        </div>
+    );
+}
