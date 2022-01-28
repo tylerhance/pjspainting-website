fa33fb7addf1120d82c49f4a777d17988a4866e8
Tyler Hance <tyler.hance@gmail.com>
2022-01-27 17:31:01 -0800

    Writing the about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,2 +1,18 @@
 import React from 'react';
 // import "../../images" from "react";
+import "../../styles/About.css";
+
+export default function About() {
+    return(
+        <div>
+            <h1>About PJs</h1>
+            <div className="about-container">
+                <p>
+                    PJ's Painting is a locally owned family business that specializes in residential and commercial painting. It was founded by Jim Hance in 2007 and has been serving the greater Pacific Northwest area since then.
+                    A strong dedication to above industry standards with relation to quality of the finished product. Quality of workmanship is a priority for PJ's Painting as well as
+                    ensuring the highest level of customer satisfaction. 
+                </p>
+            </div>
+        </div>
+    )
+}
